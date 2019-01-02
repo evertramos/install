3783ffe2a244a358d8529b55fcb63f0a2033017a
Evert Ramos <evert.ramos@gmail.com>
2019-01-02 09:12:14 -0500

    uncomment user creation

--- user/add.sh
+++ user/add.sh
@@ -5,10 +5,10 @@ echo "You must be root to run this command"
 source .env
 
 # Create the User
-#useradd -c "$USER_FULL_NAME" -U -G $GROUPS -m -s /bin/bash $NEW_USER
+useradd -c "$USER_FULL_NAME" -U -G $GROUPS -m -s /bin/bash $NEW_USER
 
 # Add New User to Sudoers
-#echo "$NEW_USER ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/90-cloud-init-users
+echo "$NEW_USER ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/90-cloud-init-users
 
 
 # Set Password for the new User
